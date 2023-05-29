6333857a6101aef399fb755d58d740dc61a94b7e
asdiop153246 <magicsiam001@gmail.com>
2023-05-29 19:21:07 +0700

    Island sinking

--- Assets/script/Movetoward.cs
+++ Assets/script/Movetoward.cs
@@ -6,10 +6,35 @@ public class Movetoward : MonoBehaviour
 {
     public float speed = 1.0f;
     public Transform target;
-    // Update is called once per frame
+    public Transform target2; // Second destination
+    private bool isFirstTarget = true;
+    private bool isWaiting = false;
+
     void Update()
     {
+        // if waiting don't update position
+        if (isWaiting)
+            return;
+
         var step = speed * Time.deltaTime; // calculate distance to move
         transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+        // Check if the object has reached its destination
+        if (Vector3.Distance(transform.position, target.position) < 0.001f)
+        {
+            if (isFirstTarget)
+            {
+                StartCoroutine(WaitAndMove(30)); // Wait for 30 seconds
+            }
+        }
+    }
+
+    IEnumerator WaitAndMove(float seconds)
+    {
+        isWaiting = true;
+        yield return new WaitForSeconds(seconds);
+        target = target2; // Change the target to the second destination
+        isFirstTarget = false;
+        isWaiting = false;
     }
 }
